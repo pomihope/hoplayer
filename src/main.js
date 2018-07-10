ca9e835440c09504cbdc2f1cd304c99a96ad1dfa
yaoandy107 <yaoandy107@gmail.com>
2018-07-11 00:43:36 +0800

    Delete custom theme setting

--- src/main.js
+++ src/main.js
@@ -6,15 +6,17 @@ import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 
-Vue.use(Vuetify, { theme: {
-  primary: '#ee44aa',
-  secondary: '#424242',
-  accent: '#82B1FF',
-  error: '#FF5252',
-  info: '#2196F3',
-  success: '#4CAF50',
-  warning: '#FFC107'
-}})
+// Vue.use(Vuetify, { theme: {
+//   primary: '#ee44aa',
+//   secondary: '#424242',
+//   accent: '#82B1FF',
+//   error: '#FF5252',
+//   info: '#2196F3',
+//   success: '#4CAF50',
+//   warning: '#FFC107'
+// }})
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
